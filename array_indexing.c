621e11432bc46603b7cf3840772544510c4b170f
robertokoba7 <okobarobert7@gmail.com>
2022-06-28 13:16:38 +0300

    array indexing

---
+++ array_indexing.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	/*Create an integer array named arr of size 10                */
+	int arr[10];
+	/*accessing element at 0 index and setting its value to 5     */
+	arr[0] = 5;
+	/*accessing and print value at 0 index we get the output as 5.*/
+	
+	printf("%d\n", arr[0]);
+
+	return (0);
+}
