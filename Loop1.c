4a71c6354c26c70267709d335896edf289e7c25b
robertokoba7 <okobarobert7@gmail.com>
2022-07-15 20:38:43 +0300

    loop trials

---
+++ Loop1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int pin = 0;
+	int tries = 0;
+
+	printf("BANK OF CODE:\n");
+	printf("Enter your PIN: ");
+	scanf("%d)", &pin);
+
+	tries++;
+
+	while (pin != 1234 && tries < 3)
+	{
+		printf("Enter your PIN: ");
+		scanf("%d", &pin);
+		tries++;
+	}
+
+	if (pin == 1234)
+	{
+		printf("PIN accepted!\n");
+		printf("You now have access.\n");
+	}
+	return (0);
+}
